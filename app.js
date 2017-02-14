2955136f1bf1dd261aefb4e0829040c77d8f29bc
Andrew Cox <drew.r.cox@gmail.com>
2017-02-14 15:52:04 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Single state object
+var state = {
+    items: []
+};
+
+// State modification functions
+var addItem = function(state, item) {
+    state.items.push(item);
+};
+
+// Render functions
+var renderList = function(state, element) {
+    var itemsHTML = state.items.map(function(item) {
+        return '<li><span class="shopping-item">' + item + '</span></li>';
+    });
+    element.html(itemsHTML);
+};
+
+var listModifiers = function() {
+  var listDiv = $('
+  <div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div>');
+  return $('.shopping-item').append(listHtml);
+};
+
+
+// Event listeners
+$('.js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    addItem(state, $('#shopping-list-entry').val());
+    renderList(state, $('.shopping-list'));
+    listModifiers();
+});
