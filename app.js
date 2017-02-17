1955eb8e5bb13ca8383104b07bf5bf0efe59e55d
Andrew Cox <drew.r.cox@gmail.com>
2017-02-17 15:41:46 -0500

    Site is now fully functional and creates an editable list

--- app.js
+++ app.js
@@ -5,31 +5,62 @@ var state = {
 
 // State modification functions
 var addItem = function(state, item) {
-    state.items.push(item);
+    state.items.push({
+      displayName: item,
+      checkedOff: false
+    });
 };
+
+function deleteItem(state, itemIndex) {
+  state.items.splice(itemIndex, 1);
+}
 
 // Render functions
 var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-        return '<li><span class="shopping-item">' + item + '</span></li>';
+    element.empty();
+    var itemsHTML = state.items.map(function(item, index) {
+        return listModifiers(item, index);
     });
-    element.html(itemsHTML);
+    element.append(itemsHTML);
 };
 
-var listModifiers = function() {
-  var listDiv = $('<div class="shopping-item-controls">' +
-                    '<button class="shopping-item-toggle">' +
-                      '<span class="button-label">check</span>' +
-                    '</button>' +
-                    '<button class="shopping-item-delete">' +
-                      '<span class="button-label">delete</span>' +
-                    '</button>' +
-                  '</div>');
-  $('.shopping-item').append(listDiv);
+var deleteClosure = function(index) {
+  return function(event) {
+    deleteItem(state, index);
+    renderList(state, $('.shopping-list'));
+  };
+};
+
+var toggleClosure = function(index) {
+  return function(event) {
+    state.items[index].checkedOff = !state.items[index].checkedOff;
+    renderList(state, $('.shopping-list'));
+  };
+};
+
+var listModifiers = function(item, index) {
+  var listDiv = $('<li>' +
+                    '<span class="shopping-item"></span>' +
+                    '<div class="shopping-item-controls">' +
+                      '<button class="shopping-item-toggle">' +
+                        '<span class="button-label">check</span>' +
+                      '</button>' +
+                      '<button class="shopping-item-delete">' +
+                        '<span class="button-label">delete</span>' +
+                      '</button>' +
+                    '</div>' +
+                 '</li>');
+  listDiv.find('.shopping-item').text(item.displayName);
+  if (item.checkedOff) {
+    listDiv.find('.shopping-item').toggleClass('shopping-item__checked');
+  }
+  listDiv.find('.shopping-item-toggle').click(toggleClosure(index));
+  listDiv.find('.shopping-item-delete').click(deleteClosure(index));
+  return listDiv;
 };
 
 // Event listeners
-$('.js-shopping-list-form').submit(function(event) {
+$('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
     addItem(state, $('#shopping-list-entry').val());
     renderList(state, $('.shopping-list'));
